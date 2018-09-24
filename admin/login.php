a47977ae62f2ad7f2c6bb02b58fb77d266158cf3
alifraher <alifraher@gmail.com>
2018-09-24 11:10:22 +0700

    Create login form

---
+++ admin/login.php
@@ -0,0 +1,145 @@
+﻿<?php 
+	include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xphp">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Sistem Informasi Futsal</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+     <!-- php5 Shiv and Respond.js for IE8 support of php5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/php5shiv/3.7.0/php5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <marquee><strong>Komplek </strong>SPBU No. 14-294-722 Vitka Point (Tiban)
+                    &nbsp;&nbsp;
+                    <strong>Telp: </strong>0778 - 326623, 326289 
+					<strong>Fax: </strong>0778 - 326389 </marquee>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+
+                    <img src="assets/img/logo.png" />
+                </a>
+
+            </div>
+
+            <div class="left-div">
+                <div class="user-settings-wrapper">
+                    <ul class="nav">
+
+                        <li class="dropdown">
+                           
+                           
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+   
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Please Login To Enter </h4>
+
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                   <form role="form" action="proseslogin.php" method="post" class="login-form">
+                     <label>Enter Username : </label>
+                        <input type="text" name="username" id="username" class="form-control" />
+                        <label>Enter Password :  </label>
+                        <input type="password" name="password" id="password" class="form-control" />
+                        <hr />
+                        <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-user"></span> &nbsp;Log In </a>&nbsp;
+						<a href="daftar.php"
+					</form>
+                </div>
+				<!--
+                <div class="col-md-6">
+                    <div class="alert alert-info">
+					<h3 class=" text-center">
+					<strong>Informasi Terbaru Seputar Transaksi Booking</strong></h3></br>
+                       <div class="bs-component">
+					 
+					<?php
+                    $tampil=mysqli_query($koneksi, "select * from tb_transaksi order by id_transaksi desc limit 3");
+                    while($data2=mysqli_fetch_array($tampil)){
+                    ?>
+					
+                      <div class="alert alert-dismissible alert-danger">
+                        <button class="close" type="button" data-dismiss="alert">×</button>
+									
+					  <strong><?php echo $data2['nama'];?></strong>, Merupakan Pemesan Baru di Sistem Vitka Futsal Center.
+                      </div>
+                            <?php } ?>						
+					 	
+                    </div>
+                       
+                    </div>
+					
+                </div>
+				-->
+
+            </div>
+        </div>
+    </div>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2017 Vitka Futsal Center | By : <a href="../index.php" target="_blank">SPAN Community</a>
+				</div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
